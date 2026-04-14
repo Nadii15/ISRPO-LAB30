6ffe5c8f632c9880357b6b84221985616e50a1e0
s0m1k111 <spavel1207@gmail.com>
2026-04-14 16:47:58 +0300

    eend

---
+++ HeroesApi/Controllers/HeroesController.cs
@@ -0,0 +1,12 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+using HeroesApi.Models;
+using HeroesApi.Data;
+
+namespace HeroesApi.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class HeroesController : ControllerBase {
+    
+}
